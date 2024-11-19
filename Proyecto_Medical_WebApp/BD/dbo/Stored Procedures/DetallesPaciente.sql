8d8147f1d127693c15e26fae321c0f612fc795f1
EliecerMS <ejmendozasoto@gmail.com>
2024-11-19 17:42:50 -0600

    update del proyecto BD

---
+++ Proyecto_Medical_WebApp/BD/dbo/Stored Procedures/DetallesPaciente.sql
@@ -0,0 +1,46 @@
+﻿CREATE PROCEDURE [dbo].[DetallesPaciente]
+	@IdPaciente int
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+		WITH UltimaMedicacionPreescrita AS (
+		SELECT 
+			mp.id_Medicacion_Paciente, 
+			m.nombre AS Medicamento, 
+			mp.fecha_Preesctrito,
+			ROW_NUMBER() OVER (ORDER BY mp.fecha_Preesctrito DESC) AS rn  
+		FROM Medicacion_Paciente mp
+		INNER JOIN Medicamento m ON mp.id_Medicamento = m.id_Medicamento
+		WHERE mp.id_Paciente = @IdPaciente  
+	),
+	EnfermedadMayorFase AS (
+		SELECT 
+			ed.id_Enfermedad, 
+			e.nombre AS Enfermedad, 
+			ed.fase_Enfermedad,
+			ROW_NUMBER() OVER (ORDER BY 
+				CASE 
+					WHEN ed.fase_Enfermedad = 'Fase 1' THEN 1
+					WHEN ed.fase_Enfermedad = 'Fase 2' THEN 2
+					WHEN ed.fase_Enfermedad = 'Fase 3' THEN 3
+					WHEN ed.fase_Enfermedad = 'Fase 4' THEN 4
+					ELSE 0  
+				END DESC) AS rn  
+		FROM Enfermedad_Diagnostico ed
+		INNER JOIN Enfermedad e ON ed.id_Enfermedad = e.id_Enfermedad
+		WHERE ed.id_Paciente = @IdPaciente  
+	)
+	SELECT 
+		ump.Medicamento,
+		emf.Enfermedad,
+		emf.fase_Enfermedad,
+		p.peso,      
+		p.estatura    
+	FROM UltimaMedicacionPreescrita ump
+	INNER JOIN EnfermedadMayorFase emf ON ump.rn = 1 AND emf.rn = 1
+	INNER JOIN Paciente p ON p.id_Paciente = @IdPaciente  
+
+END
---
+++ Proyecto_Medical_WebApp/BD/dbo/Stored Procedures/PacientesPadecimientos.sql
@@ -0,0 +1,20 @@
+﻿CREATE PROCEDURE [dbo].[PacientesPadecimientos]
+	@IdMedico int
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+	SELECT 
+		CONCAT(p.nombre, ' ', p.primer_Apellido, ' ', p.segundo_Apellido) AS NombreCompletoPaciente,
+		e.nombre AS Enfermedad, 
+		ed.id_EnferDiagnostico AS IdEnfermedadDiagnostico
+	FROM Cita c
+		INNER JOIN Paciente p ON c.id_Paciente = p.id_Paciente
+		INNER JOIN Medico m ON c.id_Medico = m.id_Medico  
+		INNER JOIN Enfermedad_Diagnostico ed ON p.id_Paciente = ed.id_Paciente  
+		INNER JOIN Enfermedad e ON ed.id_Enfermedad = e.id_Enfermedad 
+	WHERE m.id_Medico = @IdMedico  
+
+END
