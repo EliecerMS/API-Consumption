0ac5eb02ccc484e12d3aeec58869e17323eed474
EliecerMS <ejmendozasoto@gmail.com>
2024-12-03 16:16:38 -0600

    UPDATE BD VERSION 3 NUEVO SCRPIT CORREGIDO

--- BD Version 3.sql
+++ BD Version 3.sql
@@ -93,3 +93,104 @@ GO
 
 ALTER TABLE [Paciente] ADD FOREIGN KEY ([id_Paciente]) REFERENCES [Persona] ([id_Persona])
 GO
+
+
+
+
+-- CORRECIONES NUEVAS DEL SCRPIT
+
+CREATE TABLE [Medico] (
+  [id_Medico] uniqueidentifier PRIMARY KEY,
+  [especialidad] nvarchar(50) NOT NULL
+)
+GO
+
+CREATE TABLE [Paciente] (
+  [id_Paciente] uniqueidentifier PRIMARY KEY,
+  [peso] int NOT NULL,
+  [estatura] int NOT NULL
+)
+GO
+
+CREATE TABLE [Cita] (
+  [id_Cita] int IDENTITY(1,1) PRIMARY KEY,
+  [id_Medico] uniqueidentifier,
+  [id_Paciente] uniqueidentifier NOT NULL,
+  [notas_Cita] nvarchar(MAX),
+  [fecha_Cita] DATE NOT NULL,
+  [atendida] BIT NOT NULL,
+  [especialidad] nvarchar(50) NOT NULL,
+  [motivo] nvarchar(50) NOT NULL
+)
+GO
+
+CREATE TABLE [Enfermedad_Diagnostico] (
+  [id_EnferDiagnostico] int IDENTITY(1,1) PRIMARY KEY,
+  [id_Cita] int NOT NULL,
+  [id_Enfermedad] int NOT NULL,
+  [notas_Diagnostico] nvarchar(MAX) NOT NULL,
+  [fase_Enfermedad] nvarchar(50) NOT NULL,
+  [fecha_Diagnostico] DATE NOT NULL
+)
+GO
+
+CREATE TABLE [Medicacion_Paciente] (
+  [id_Medicacion_Paciente] int IDENTITY(1,1) PRIMARY KEY,
+  [id_Medicamento] int NOT NULL,
+  [id_Cita] int NOT NULL,
+  [dosis] nvarchar(50) NOT NULL,
+  [intrucciones] nvarchar(50) NOT NULL,
+  [fecha_Preesctrito] DATE NOT NULL
+)
+GO
+
+CREATE TABLE [Medicamento] (
+  [id_Medicamento] int IDENTITY(1,1) PRIMARY KEY,
+  [nombre] nvarchar(50)
+)
+GO
+
+CREATE TABLE [Enfermedad] (
+  [id_Enfermedad] int IDENTITY(1,1) PRIMARY KEY,
+  [nombre] nvarchar(50)
+)
+GO
+
+CREATE TABLE [Persona] (
+  [id_Persona] uniqueidentifier PRIMARY KEY,
+  [nombre] nvarchar(50) NOT NULL,
+  [primer_Apellido] nvarchar(50) NOT NULL,
+  [segundo_Apellido] nvarchar(50) NOT NULL,
+  [fecha_Nacimiento] DATE NOT NULL,
+  [genero] nvarchar(50) NOT NULL,
+  [email] nvarchar(50) NOT NULL,
+  [telefono] nvarchar(50) NOT NULL,
+  [edad] nvarchar(50) NOT NULL,
+  [identificacion] nvarchar(50) NOT NULL,
+  [rol] nvarchar(50) NOT NULL
+)
+GO
+
+ALTER TABLE [Cita] ADD FOREIGN KEY ([id_Paciente]) REFERENCES [Paciente] ([id_Paciente])
+GO
+
+ALTER TABLE [Cita] ADD FOREIGN KEY ([id_Medico]) REFERENCES [Medico] ([id_Medico])
+GO
+
+ALTER TABLE [Medicacion_Paciente] ADD FOREIGN KEY ([id_Cita]) REFERENCES [Cita] ([id_Cita])
+GO
+
+ALTER TABLE [Enfermedad_Diagnostico] ADD FOREIGN KEY ([id_Cita]) REFERENCES [Cita] ([id_Cita])
+GO
+
+ALTER TABLE [Medicacion_Paciente] ADD FOREIGN KEY ([id_Medicamento]) REFERENCES [Medicamento] ([id_Medicamento])
+GO
+
+ALTER TABLE [Enfermedad_Diagnostico] ADD FOREIGN KEY ([id_Enfermedad]) REFERENCES [Enfermedad] ([id_Enfermedad])
+GO
+
+ALTER TABLE [Medico] ADD FOREIGN KEY ([id_Medico]) REFERENCES [Persona] ([id_Persona])
+GO
+
+ALTER TABLE [Paciente] ADD FOREIGN KEY ([id_Paciente]) REFERENCES [Persona] ([id_Persona])
+GO
