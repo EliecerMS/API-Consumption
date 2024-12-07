55f83874c3b45d58e724f39ef74a38e0e9f80478
EliecerMS <ejmendozasoto@gmail.com>
2024-12-07 10:43:37 -0600

    estructura de razor pages para las pantallas del medico

---
+++ Proyecto_Medical_App_Parte_Web/Proyecto_Medical_App_Parte_Web/Pages/Medico/Citas/FinalizarCita.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Proyecto_Medical_App_Parte_Web.Pages.Medico.Citas
+{
+    public class FinalizarCitaModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ Proyecto_Medical_App_Parte_Web/Proyecto_Medical_App_Parte_Web/Pages/Medico/Diagnosticos/DetallesDiagnostico.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Proyecto_Medical_App_Parte_Web.Pages.Medico.Diagnosticos
+{
+    public class DetallesDiagnosticoModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
